3657f1b01f3e9b68c83baa966258dfc778d0285e
TY <86838308+foo-mr@users.noreply.github.com>
2022-09-25 14:06:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     if (input.lightLevel() < 80) {
-        pins.digitalWritePin(DigitalPin.P2, 180)
+        pins.servoWritePin(AnalogPin.P2, 180)
     } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
 })
